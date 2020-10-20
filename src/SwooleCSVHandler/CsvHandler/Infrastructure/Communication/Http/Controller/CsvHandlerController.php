609b44629781b6b4a3084ffac6cb87cc6ec2e790
Matteo <m.galacci@gmail.com>
2020-10-20 20:00:51 +0200

    wip: csv handler

--- src/SwooleCSVHandler/CsvHandler/Infrastructure/Communication/Http/Controller/CsvHandlerController.php
+++ src/SwooleCSVHandler/CsvHandler/Infrastructure/Communication/Http/Controller/CsvHandlerController.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 namespace SwooleCSVHandler\CsvHandler\Infrastructure\Communication\Http\Controller;
 
+use Co\System;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+
 class CsvHandlerController
 {
-
+    public function getHandleCsv(Response $response, Request $request): void
+    {
+        //System::sleep(2);
+        echo 'foo';
+        $response->end('<html><body><h1>Ci sono</h1></body></html>');
+    }
 }
