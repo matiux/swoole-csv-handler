60fd66c5ff66058a3c1fff54e6ebd9dbeade4457
Guido Porcaro <guich96@gmail.com>
2020-10-30 01:24:43 +0100

    chore: add header component
    Added a base header component

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,8 @@
+/**
+ * The Header of the project
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const Header = () => <header className="header">{process.env.REACT_APP_SITE_NAME}</header>
+
+export default Header
